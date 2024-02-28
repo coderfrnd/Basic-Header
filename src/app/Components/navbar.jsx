2d65454a029ac3c27f0299efb293c658e8ae4d36
Keshav Kartik Sharma <keshav4762@gmail.com>
2024-02-28 23:33:00 +0530

    routing done

--- src/app/Components/navbar.jsx
+++ src/app/Components/navbar.jsx
@@ -1,6 +1,17 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
 import React from 'react'
+useRouter
 
 function Navbar() {
+
+  const router = useRouter();
+
+  const navigate= (name)=>{
+    router.push(name)
+  }
+
   return (
     <nav className='flex justify-between w-full bg-black text-white p-4 text-ls'>
         <div className='flex items-center'>
@@ -12,8 +23,8 @@ function Navbar() {
         </div>
 
         <div className='flex justify-between'>
-          <button className='mr-10 bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded-2xl'>Register</button>
-          <button className='mr-10 bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded-2xl'>Login</button>
+          <button className='mr-10 bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded-2xl' onClick={()=>{navigate('/register')}}>Register</button>
+          <button className='mr-10 bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded-2xl' onClick={()=>{navigate('/login')}}>Login</button>
         </div>
     </nav>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
       <Navbar />
+      
     </main>
   );
 }
